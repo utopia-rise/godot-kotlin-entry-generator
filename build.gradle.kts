65364541928f1f2e464e990e15fba3a2cd025c2d
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-08-22 17:47:17 +0200

    Setup project with entry generator from `godot-kotlin`

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    kotlin("jvm")
+    `maven-publish`
+    id("org.ajoberstar.grgit") version "4.0.2"
+}
+
+group = "com.utopia-rise"
+version = "0.1.0-${DependenciesVersions.godotVersion}"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly(kotlin("compiler"))
+    implementation("com.squareup:kotlinpoet:${DependenciesVersions.kotlinPoetVersion}")
+}
+
+tasks {
+    val sourceJar by creating(Jar::class) {
+        archiveBaseName.set(project.name)
+        archiveVersion.set(project.version.toString())
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    build {
+        finalizedBy(publishToMavenLocal)
+    }
+}
+
+publishing {
+    publications {
+        val godotEntryGenerator by creating(MavenPublication::class) {
+            pom {
+                groupId = "${project.group}"
+                artifactId = project.name
+                version = "${project.version}"
+            }
+            from(components.getByName("java"))
+            artifact(tasks.getByName("sourceJar"))
+        }
+    }
+}
+
+val currentCommit = grgit.head()
+// check if the current commit is tagged
+var releaseMode = grgit.tag.list().firstOrNull { tag -> tag.commit.id == currentCommit.id } != null
+
+project.extra["artifacts"] = arrayOf("godotEntryGenerator")
+extra["releaseMode"] = releaseMode
+
+
+apply {
+    plugin(BintrayPublish::class.java)
+}
---
+++ buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -0,0 +1,4 @@
+object DependenciesVersions {
+    const val kotlinPoetVersion: String = "1.5.0"
+    const val godotVersion: String = "3.2"
+}
