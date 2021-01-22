625e4827de3f593e690660a7d8de4f970d82e774
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-01-22 12:17:23 +0100

    Implement registeredClassName check

--- src/main/kotlin/godot/entrygenerator/generator/clazz/JvmClassRegistrationGenerator.kt
+++ src/main/kotlin/godot/entrygenerator/generator/clazz/JvmClassRegistrationGenerator.kt
@@ -4,23 +4,31 @@ import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
 import godot.entrygenerator.EntryGenerationType
+import godot.entrygenerator.EntryGenerator
+import godot.entrygenerator.extension.getAnnotationValue
 import godot.entrygenerator.generator.function.FunctionRegistrationGeneratorProvider
 import godot.entrygenerator.generator.property.PropertyRegistrationGeneratorProvider
 import godot.entrygenerator.generator.signal.SignalRegistrationGeneratorProvider
 import godot.entrygenerator.model.ClassWithMembers
+import godot.entrygenerator.model.REGISTER_CLASS_ANNOTATION
+import godot.entrygenerator.model.REGISTER_CLASS_ANNOTATION_CUSTOM_NAME_ARGUMENT
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import godot.entrygenerator.model.RegisteredProperty
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
 import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 
 class JvmClassRegistrationGenerator : ClassRegistrationGenerator() {
 
     override fun provideRegisterClassControlFlow(classWithMembers: ClassWithMembers, classRegistryControlFlow: FunSpec.Builder, className: ClassName, superClass: String, godotBaseClass: String, isTool: Boolean): FunSpec.Builder {
+        val classNameAsString = getClassNameAsString(classWithMembers.classDescriptor)
+        EntryGenerator.registeredClassNames.add(classWithMembers.classDescriptor.fqNameSafe.asString() to classNameAsString)
         classRegistryControlFlow.beginControlFlow(
-            "registerClass<%T>(%T::class.qualifiedName!!,·%S,·$isTool,·%S)·{",
+            "registerClass<%T>(%S,·%S,·$isTool,·%S)·{",
             className,
-            className,
+            classNameAsString,
             superClass,
             godotBaseClass
         )
@@ -46,6 +54,17 @@ class JvmClassRegistrationGenerator : ClassRegistrationGenerator() {
         }
 
         return classRegistryControlFlow
+    }
+
+    private fun getClassNameAsString(classDescriptor: ClassDescriptor): String {
+        val customClassName = classDescriptor.annotations.getAnnotationValue(REGISTER_CLASS_ANNOTATION, REGISTER_CLASS_ANNOTATION_CUSTOM_NAME_ARGUMENT, "")
+        return if (customClassName.isNotEmpty()) {
+            customClassName
+        } else {
+            val packagePath = classDescriptor.fqNameSafe.parent().asString()
+            val simpleName = classDescriptor.fqNameSafe.shortName().asString()
+            "${packagePath.replace(".", "_")}_$simpleName"
+        }
     }
 
     override fun registerFunctions(functions: List<FunctionDescriptor>, registerClassControlFlow: FunSpec.Builder, className: ClassName) {
