ff3f53b7927c501995d8e5a27672a48fada78207
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2021-02-15 17:47:46 +0100

    Feature/store kotlin class in script (#16)
    * Add registration of user class
    
    * Fix filepath vs package check for windows
    
    Co-authored-by: Cedric Hippmann <cedric.hippmann@hotmail.com>

--- src/main/kotlin/godot/entrygenerator/EntryGenerator.kt
+++ src/main/kotlin/godot/entrygenerator/EntryGenerator.kt
@@ -231,10 +231,11 @@ object EntryGenerator {
             .filter { ktClass ->
                 var containingFilePath = ktClass.containingKtFile.virtualFile.path
                 srcDirs.forEach { srcDir ->
-                    containingFilePath = containingFilePath.removePrefix(srcDir)
+                    // Replace separator by unix separator because of windows file system
+                    containingFilePath = containingFilePath.removePrefix(srcDir.replace(File.separatorChar, '/'))
                 }
-                containingFilePath = containingFilePath.removePrefix(File.separator).removeSuffix(".kt")
-                val packagePath = ktClass.fqName?.asString()?.replace(".", File.separator)
+                containingFilePath = containingFilePath.removePrefix("/").removeSuffix(".kt")
+                val packagePath = ktClass.fqName?.asString()?.replace('.', '/')
                 packagePath != containingFilePath
             }
             .toSet()
